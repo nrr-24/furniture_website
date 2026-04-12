17d2720f903fccd29b6ffc4775cd48004e4729f7
nrr-24 <nouranmohamedclg@gmail.com>
2026-04-12 15:49:34 +0200

    :p

--- furniture-store/app/api/upload/route.ts
+++ furniture-store/app/api/upload/route.ts
@@ -1,6 +1,8 @@
 import { supabase } from '../../../lib/supabase';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get('filename');
